592d8e077fb6826893a9f6e87316bdd7fa2e77ee
Hamit CANPOLAT <hhamitc@gmail.com>
2024-11-10 19:38:53 +0300

    Add project files.

---
+++ 01_ LinqPractice/Program.cs
@@ -0,0 +1,108 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Liste oluşturma
+        List<int> NumberList = new List<int>();
+
+        //Rasthele sayı oluşturmak için Random sınıfından üretme
+        Random random = new Random();
+
+
+        // 10 Adet sayıyı for ile üretip Listeye ekleme.
+        for (int i = 0; i < 10; i++)
+        {
+            int number = random.Next(-25, 26);
+            NumberList.Add(number);
+        }
+        //Üretilen listeyi yazdırma.
+        Console.Write("{0,-20}: ", "Tam Liste");
+        foreach (var item in NumberList)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine("\n----------------------");
+
+
+
+        //Üretilen listeden çift sayıları filtreleme 
+        var EvenNumbers = NumberList.Where(num => num % 2 == 0);
+        //Filtrelenen sayıları yazdırma.
+        Console.Write("{0,-20}: ", "Çift Sayılar");
+        foreach (var item in EvenNumbers)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine("\n----------------------");
+
+
+
+        // ÜRetilen tek sayıları yazdırma
+        var OddNumbers = NumberList.Where(num => num % 2 != 0);
+        //Filtrelenen sayıları yazdırma.
+        Console.Write("{0,-20}: ", "Tek Sayılar");
+        foreach (var item in OddNumbers)
+        {
+            Console.Write($"{item} ");
+        }
+        Console.WriteLine("\n----------------------");
+
+
+
+        //Negatif Sayıları Filtreleme
+        var NegativeNumbers = from number in NumberList
+                              where number < 0
+                              select number;
+        //Filtrelenen numaraları yazdırma.
+        Console.Write("{0,-20}: ", "Negatif Sayılar");
+        foreach (var item in NegativeNumbers)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine("\n----------------------");
+
+
+
+        //Pozitif Sayıları Filtreleme
+        var PositiveNumbers = NumberList.Where(num => num > 0);
+        //Filtrelenen Sayıları Yazdırma
+        Console.Write("{0,-20}: ", "Pozitif Sayılar");
+        foreach (var item in PositiveNumbers)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine("\n----------------------");
+
+
+
+        // 15'den büyük ve 22 den küçük sayıları filtreleme
+        var SelectedAreaNumbers = NumberList.Where(num => num > 15 && num < 22);
+        //Filtrelenen Sayıları Yazdırma
+        Console.Write("{0,-20}: ", "15-22 Arasındakiler");
+        foreach (var item in SelectedAreaNumbers)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine("\n----------------------");
+
+
+
+        //Listedeki her elemanın karesini alıp yeni liste oluşturma
+        var SquaredNumbers = NumberList.Select(num => num * num).ToList();
+
+        //Yeni Listeyi yazdırma;
+        Console.Write("{0,-20}: ", "Sayıların Kareleri:");
+        foreach (var item in SquaredNumbers)
+        {
+            Console.Write($"{item} ");
+        }
+
+
+
+
+
+
+
+        Console.ReadKey();
+    }
+}
---
+++ 01_ LinqPractice/README.md
@@ -0,0 +1,14 @@
+﻿# Patika+ Back-End Web Development Kursu 7. Hafta Linq Pratiği Ödevi
+
+Aşağıdaki isterleri cevaplamaktadır.
+
+---
+
+### Görev: Rastgele 10 adet sayıdan oluşan bir liste oluşturunuz. Bu liste üzerinden aşağıdaki LINQ sorgularını çalıştırarak konsol ekranına istenilenleri yazdırınız.
+
+1. **Çift olan sayılar**
+2. **Tek olan sayılar**
+3. **Negatif sayılar**
+4. **Pozitif sayılar**
+5. **15'ten büyük ve 22'den küçük sayılar**
+6. **Listedeki her bir sayının karesi (Bunun için yeni bir liste oluşturup yazdırabilirsiniz.)**
